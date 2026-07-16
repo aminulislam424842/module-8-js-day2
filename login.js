14f8ae02486b5a6985d88d387b60088cfe1c2c28
Aminul islam <aminulislam424842@gmail.com>
2026-07-16 23:14:31 +0600

    feat: implement simple login validation

---
+++ login.js
@@ -0,0 +1,12 @@
+const username="aminul";
+const password="aminul4248";
+
+let checkUserName="aminul" ;
+let checkPassword="aminul424" ;
+
+if ((username===checkUserName)&&(password===checkPassword)){
+  console.log("login successful");
+}
+else{
+  console.log("Login Unsuccessful");
+}
