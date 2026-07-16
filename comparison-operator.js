536cf13d322539f3025f5657843614e11f7865cb
Aminul islam <aminulislam424842@gmail.com>
2026-07-16 20:48:10 +0600

    feat: practice JavaScript comparison operators

---
+++ comparison-operator.js
@@ -0,0 +1,33 @@
+let price1 = 739;
+let price2 = 50;
+
+if (price1 > price2) {
+  console.log("true");
+}
+
+if (price1 < price2) {
+  console.log("false");
+}
+
+if (price1 >= price2) {
+  console.log("true");
+}
+
+if (price1 <= price2){
+  console.log("false");
+}
+
+if(price1 == 739){
+  console.log("true");
+}
+
+if(50 === price2){
+  console.log("true");
+}
+
+if ( price1 !== price2) {
+  console.log("true");
+}
+if ( price1 != price2) {
+  console.log("true");
+}
