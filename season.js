530bef5e87f7fb91af21ebd7ba49b50999290b37
Aminul islam <aminulislam424842@gmail.com>
2026-07-17 14:50:19 +0600

    feat: determine season based on month

---
+++ season.js
@@ -0,0 +1,29 @@
+let month = 1;
+
+if (month === 2 || month === 3) {
+  console.log("Spring");
+}
+
+else if (month === 4 || month === 5) {
+  console.log("Summer");
+}
+
+else if (month === 6 || month === 7 || month === 8) {
+  console.log("Monsoon");
+}
+
+else if (month === 9 || month === 10) {
+  console.log("Autumn");
+}
+
+else if (month === 11) {
+  console.log("Late Autumn / Hemanta");
+}
+
+else if (month === 12 || month === 1) {
+  console.log("Winter");
+}
+
+else {
+  console.log("Enter Valid Month");
+}
