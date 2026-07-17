ed6904fe20c118c9522cbb7f2ff19f625ece4124
Aminul islam <aminulislam424842@gmail.com>
2026-07-17 14:40:02 +0600

    feat: implement BMI category checker

---
+++ bmi.js
@@ -0,0 +1,14 @@
+let bmiVariable = 24.9;
+
+if (bmiVariable <= 18.5) {
+  console.log("Underweight");
+}
+else if (bmiVariable <= 24.9) {
+  console.log("Healthy Weight");
+}
+else if (bmiVariable <= 29.9) {
+  console.log("Overweight");
+}
+else {
+  console.log("Obese");
+}
