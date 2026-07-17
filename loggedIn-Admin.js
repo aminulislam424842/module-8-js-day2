b1fce6112914027878dc956563656e9fa2a3c154
Aminul islam <aminulislam424842@gmail.com>
2026-07-17 15:05:33 +0600

    feat: implement role-based dashboard access

---
+++ loggedIn-Admin.js
@@ -0,0 +1,14 @@
+let isLoggedIn = true;
+let isAdmin = true;
+
+if (isLoggedIn) {
+  if (isAdmin) {
+    console.log("Admin Dashboard");
+  }
+  else {
+    console.log("User Dashboard");
+  }
+}
+else {
+  console.log("Please Login.")
+}
