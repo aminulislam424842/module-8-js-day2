8e5bc75d321f68cca9e2ffb5fdc40b2b2020dd9c
Aminul islam <aminulislam424842@gmail.com>
2026-07-17 14:58:45 +0600

    feat: implement voting eligibility checker

---
+++ vote.js
@@ -0,0 +1,14 @@
+let age = 18;
+let hasID = true;
+
+if (age >= 18) {
+  if (hasID) {
+    console.log("Eligible to Vote");
+  }
+  else {
+    console.log("Your ID is incorrect.");
+  }
+}
+else {
+  console.log("You aren't old enough to vote yet.");
+}
