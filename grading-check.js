ab55630c1309715d3b59adaef963be1b31b498db
Aminul islam <aminulislam424842@gmail.com>
2026-07-17 14:29:42 +0600

    feat: implement student grade calculator

---
+++ grading-check.js
@@ -0,0 +1,20 @@
+let mark = 5;
+
+if (mark >= 80) {
+  console.log("A+");
+}
+else if (mark >= 70) {
+  console.log("A");
+}
+else if (mark >= 60) {
+  console.log("B");
+}
+else if (mark >= 50) {
+  console.log("C");
+}
+else if (mark >= 40) {
+  console.log("D");
+}
+else {
+  console.log("F");
+}
